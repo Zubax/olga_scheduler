3fed86edffd78a392797ec49071accc08cfff8d7
Pavel Kirienko <pavel.kirienko@gmail.com>
2026-02-05 01:06:54 +0200

    cavl2.h

--- include/olga_scheduler/olga_scheduler.h
+++ include/olga_scheduler/olga_scheduler.h
@@ -1,5 +1,7 @@
 /// Source: https://github.com/zubax/olga_scheduler
 ///
+/// A single-file header-only EDF scheduler for embedded applications.
+///
 /// Copyright (c) Zubax Robotics  <info@zubax.com>
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
@@ -18,6 +20,9 @@
 #pragma once
 
 #include <cavl.h> // Add to your include paths: https://github.com/pavel-kirienko/cavl
+
+#include <assert.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 // This is, strictly speaking, useless because we do not define any functions with external linkage here,
@@ -46,11 +51,28 @@ typedef struct olga_t
     int64_t (*now)(void* user);
 } olga_t;
 
+/// Current state assessment returned from olga_spin().
+typedef struct olga_spin_result_t
+{
+    int64_t next_deadline;
+    int64_t worst_lateness;
+    int64_t now;
+} olga_spin_result_t;
+
 /// To deinitialize, simply cancel all events; nothing else needs to be done.
 /// The time units can be arbitrary.
 static inline void olga_init(olga_t* const self, void* const user, int64_t (*const now)(void* user))
 {
+    assert(self != NULL);
+    assert(now != NULL);
     // TODO implement
+}
+
+static inline CAVL2_RELATION olga_private_compare_deadline(const void* user, const CAVL2_T* event)
+{
+    const int64_t outer = *(const int64_t*)user;
+    const int64_t inner = ((const olga_event_t*)event)->deadline;
+    return (outer == inner) ? 0 : ((outer > inner) ? +1 : -1);
 }
 
 /// Schedule a one-time event.
@@ -61,21 +83,27 @@ static inline void olga_defer(olga_t* const self,
                               void (*const handler)(void* user, int64_t now),
                               olga_event_t* const out_event)
 {
-    // TODO implement
+    assert(self != NULL);
+    assert(handler != NULL);
+    assert(out_event != NULL);
+    // TODO schedule the event
 }
 
 /// No effect if the event has already been completed.
 static inline void olga_cancel(olga_t* const self, olga_event_t* const event)
 {
-    // TODO implement
+    assert(self != NULL);
+    assert(event != NULL);
+    // TODO use cavl2_remove_if().
 }
 
 /// Execute pending events strictly in the order of their deadlines until there are no pending events left.
-/// Returns the time of the next pending event deadline, which is always in the future.
+/// Events with the same deadline are executed in the FIFO order.
 /// This method should be invoked regularly to pump the event loop.
-static inline int64_t olga_spin(olga_t* const self)
+static inline olga_spin_result_t olga_spin(olga_t* const self)
 {
-    // TODO implement; use cavl2_lower_bound() or similar.
+    assert(self != NULL);
+    // TODO spin until no ready events left, return the deadline of the soonest
 }
 
 #ifdef __cplusplus
