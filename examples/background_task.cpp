15e834ac8a3a09852da99c6d36e275cc721e1bb5
Maksim Drachov <maksim.drachov@outlook.com>
2026-07-01 17:00:02 +0300

    fix

--- examples/background_task.cpp
+++ examples/background_task.cpp
@@ -5,19 +5,27 @@
 #include <iostream>
 #include <thread>
 
+namespace {
+
+constexpr auto          CounterInterval       = std::chrono::seconds{ 1 };
+constexpr std::uint64_t CounterLimit          = 10;
+constexpr auto          BackgroundTaskRuntime = std::chrono::milliseconds{ 50 };
+constexpr int           BackgroundStepsPerRun = 3;
+
+} // namespace
+
 int main()
 {
-    using namespace std::chrono_literals;
     using Clock = std::chrono::steady_clock;
 
     olga_scheduler::EventLoop<Clock> loop;
     std::uint64_t                    counter    = 0;
     std::uint64_t                    bg_counter = 0;
 
-    auto evt = loop.repeat(1s, [&](const auto& arg) {
+    auto evt = loop.repeat(CounterInterval, [&](const auto& arg) {
         ++counter;
         std::cout << "counter=" << counter << " now=" << arg.approx_now.time_since_epoch().count() << '\n';
-        if (counter > 10) {
+        if (counter > CounterLimit) {
             arg.event.cancel();
         }
     });
@@ -25,14 +33,14 @@ int main()
     auto run_background_step = [&] {
         ++bg_counter;
         std::cout << "bg_counter=" << bg_counter << '\n';
-        std::this_thread::sleep_for(50ms);
+        std::this_thread::sleep_for(BackgroundTaskRuntime);
     };
 
     while (!loop.isEmpty()) {
         const auto spin_result = loop.spin();
 
         if ((counter > 0) && !loop.isEmpty()) {
-            for (int i = 0; (i < 3) && (Clock::now() < spin_result.next_deadline); ++i) {
+            for (int i = 0; (i < BackgroundStepsPerRun) && (Clock::now() < spin_result.next_deadline); ++i) {
                 run_background_step();
             }
         }
